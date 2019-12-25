8c76aa97b27c5116f26a1f1f03f089c854bf81f4
Vignesh A S <58910726+asvignesh-qae@users.noreply.github.com>
2019-12-25 14:36:56 +0530

    Add files via upload

--- FirstDuplicateNArr.java
+++ FirstDuplicateNArr.java
@@ -1,32 +1,55 @@
-package codeSignalPacka;
+package interviewPracticesArrays;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class FirstDuplicateNArr {
-
+	/*int index =-1, fDup;
 	public int findDuplicate(int[] ar) {
-		int dup = ar[0];
-		System.out.println("The length is "+ar.length);
 		for(int i=0;i<ar.length;i++) {
-			System.out.println("i is "+i+" and its value is "+ar[i]);
-			for(int j=i+1;i<ar.length-1;j++) {
-				
-				System.out.println("j is "+j+" and its value is "+ar[j]);
-				System.out.println("----------");
+			for(int j=i+1;j<ar.length;j++) {
 				if(ar[i]==ar[j]) {
-					dup=ar[i];
-					break;
+					index = i;
+					fDup = j-i;
+					System.out.println("duplicate length is "+fDup);
+					index = fDup<=j-i?i:-1;
+					System.out.println("The index is "+index);
+					System.out.println("The first duplicate element is "+ar[i]);
+					if(fDup==1) {
+						break;
+					}
 				}
-				else
-					dup=-1;
+			}
+			if(fDup==1) {
+				break;
 			}
 		}
-		return dup;
+		return index<0?-1:ar[index];*/
+int index=-1;
+public int findDuplicate(int[] ar) {
+	Set h = new HashSet();
+	for(int i=0;i<ar.length;i++) {
+		if(h.contains(ar[i])) {
+			index = i;
+			break;
+		}
+		else {
+			h.add(ar[i]);
+		}
 	}
+	return index<0?-1:ar[index];
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FirstDuplicateNArr ob = new FirstDuplicateNArr();
-		int[] arr1 = {2, 1, 3, 5, 3, 2};
+		int[] arr1 = {8, 4, 6, 2, 6, 4, 7, 9, 5, 8};
+		//int[] arr1 = {1, 1, 2, 2, 1};
+		//int[] arr1 = {2, 1, 3, 5, 3, 2};
+		//int[] arr1 = {2, 2};
+		//int[] arr1 = {2, 4, 3, 5, 1};
+		//int[] arr1 = {1};
 		int dupli = ob.findDuplicate(arr1);
-		System.out.println("The Duplicate is "+dupli);
+		System.out.println("The first nearest Duplicate is "+dupli);
 	}
 
 }
